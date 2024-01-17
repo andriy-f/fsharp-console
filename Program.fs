d2ab4103c4a94b343d6f7320b20a1f15dec8318f
Andriy F <andriy@localhost.localdomain>
2024-01-17 13:46:16 +0100

    initial commit

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+let a = 1
+let b = 2
+let plus3 = fun x -> x + 3
+let plus3Mul5 = fun x -> (x + 3) * 5
+let mul2 = ( * ) 2
+
+printfn "Hello from F#"
