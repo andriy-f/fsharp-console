a79b92a6da8589d7320df1d38fb0aa933207e195
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-01-17 13:51:35 +0000

    more examples

--- Program.fs
+++ Program.fs
@@ -4,5 +4,7 @@ let b = 2
 let plus3 = fun x -> x + 3
 let plus3Mul5 = fun x -> (x + 3) * 5
 let mul2 = ( * ) 2
+let mul2Plus3 = mul2 >> plus3
 
 printfn "Hello from F#"
+printfn "3 * 2 + 3 = %i" (mul2Plus3 3)
